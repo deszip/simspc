e937cc928c763ee2b314b031536bf684d5f15a62
Deszip <raf.rafiki@gmail.com>
2018-10-17 09:45:15 +0300

    Initial Commit

---
+++ simspc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  simspc
+//
+//  Created by Deszip on 17/10/2018.
+//  Copyright © 2018 SimSpace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
